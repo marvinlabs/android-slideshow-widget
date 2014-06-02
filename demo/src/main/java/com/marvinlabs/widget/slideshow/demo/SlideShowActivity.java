7184bd6373e9e9231fc5c36cecf0150dc9f0c5ea
Vincent Mimoun-Prat <vincent.prat@marvinlabs.com>
2014-06-02 20:05:01 +0200

    - First attempt at a plugin to load images with the Picasso library (not yet working) - Demo changed a bit to show off some new transition, XML parameters, ...

--- demo/src/main/java/com/marvinlabs/widget/slideshow/demo/SlideShowActivity.java
+++ demo/src/main/java/com/marvinlabs/widget/slideshow/demo/SlideShowActivity.java
@@ -14,7 +14,8 @@ import com.marvinlabs.widget.slideshow.SlideShowAdapter;
 import com.marvinlabs.widget.slideshow.SlideShowView;
 import com.marvinlabs.widget.slideshow.adapter.RemoteBitmapAdapter;
 import com.marvinlabs.widget.slideshow.adapter.ResourceBitmapAdapter;
-import com.marvinlabs.widget.slideshow.playlist.RandomPlayList;
+import com.marvinlabs.widget.slideshow.picasso.PicassoBitmapAdapter;
+import com.squareup.picasso.Picasso;
 
 import java.util.Arrays;
 
@@ -44,10 +45,23 @@ public class SlideShowActivity extends Activity {
                 "http://lorempixel.com/1280/720/nature",
                 "http://lorempixel.com/1280/720/people",
                 "http://lorempixel.com/1280/720/city",
-            };
+        };
         BitmapFactory.Options opts = new BitmapFactory.Options();
         opts.inSampleSize = 2;
         adapter = new RemoteBitmapAdapter(this, Arrays.asList(slideUrls), opts);
+        return adapter;
+    }
+
+    private SlideShowAdapter createPicassoAdapter() {
+        Picasso.with(this).setLoggingEnabled(true);
+
+        String[] slideUrls = new String[]{
+                "http://lorempixel.com/1280/720/sports",
+                "http://lorempixel.com/1280/720/nature",
+                "http://lorempixel.com/1280/720/people",
+                "http://lorempixel.com/1280/720/city",
+        };
+        adapter = new PicassoBitmapAdapter(this, Arrays.asList(slideUrls));
         return adapter;
     }
 
@@ -69,18 +83,22 @@ public class SlideShowActivity extends Activity {
     protected void onStop() {
         if (adapter instanceof RemoteBitmapAdapter) {
             ((RemoteBitmapAdapter) adapter).stopAllDownloads();
+        } else if (adapter instanceof PicassoBitmapAdapter) {
+            Picasso.with(this).shutdown();
         }
         super.onStop();
     }
 
     private void startSlideShow() {
         // Create an adapter
-        // slideShowView.setAdapter(createResourceAdapter());
-        slideShowView.setAdapter(createRemoteAdapter());
+        slideShowView.setAdapter(createResourceAdapter());
+        // slideShowView.setAdapter(createRemoteAdapter());
+        // slideShowView.setAdapter(createPicassoAdapter());
 
         // Optional customisation follows
-        // slideShowView.setTransitionFactory(new SlideAndZoomTransitionFactory(2000));
-        slideShowView.setPlaylist(new RandomPlayList());
+        // slideShowView.setTransitionFactory(new RandomTransitionFactory());
+        // slideShowView.setTransitionFactory(new FlipTransitionFactory());
+        // slideShowView.setPlaylist(new RandomPlayList());
 
         // Some listeners if needed
         slideShowView.setOnSlideShowEventListener(slideShowListener);
