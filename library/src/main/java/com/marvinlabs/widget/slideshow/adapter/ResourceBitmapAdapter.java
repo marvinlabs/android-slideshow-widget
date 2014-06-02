62cf3e06a72d6022df3ab648c7f74d3bdb99b9e3
Vincent Mimoun-Prat <vincent.prat@marvinlabs.com>
2014-06-02 20:02:03 +0200

    Aync loading of resources

--- library/src/main/java/com/marvinlabs/widget/slideshow/adapter/ResourceBitmapAdapter.java
+++ library/src/main/java/com/marvinlabs/widget/slideshow/adapter/ResourceBitmapAdapter.java
@@ -3,6 +3,9 @@ package com.marvinlabs.widget.slideshow.adapter;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.util.SparseArray;
 
 /**
  * A BitmapAdapter that loads them from application resources.
@@ -13,6 +16,9 @@ public class ResourceBitmapAdapter extends BitmapAdapter {
 
     // The options to pass to the BitmapFactory used to decode the bitmaps
     private BitmapFactory.Options bitmapFactoryOptions;
+
+    // A pool of running AsyncTasks
+    private SparseArray<LoadResourceTask> runningTasks;
 
     // The resource IDs of the images to load
     private int[] slideImageIds;
@@ -43,6 +49,7 @@ public class ResourceBitmapAdapter extends BitmapAdapter {
         super(context);
         this.bitmapFactoryOptions = bitmapFactoryOptions;
         this.slideImageIds = slideImageIds;
+        this.runningTasks = new SparseArray<LoadResourceTask>(5);
     }
 
     //==============================================================================================
@@ -72,11 +79,75 @@ public class ResourceBitmapAdapter extends BitmapAdapter {
     protected void loadBitmap(int position) {
         if (position < 0 || position >= slideImageIds.length) onBitmapNotAvailable(position);
 
-        Bitmap bm = BitmapFactory.decodeResource(getContext().getResources(), slideImageIds[position], bitmapFactoryOptions);
-        if (bm != null) {
-            onBitmapLoaded(position, bm);
-        } else {
-            onBitmapNotAvailable(position);
+        // If a task is already running, cancel it
+        LoadResourceTask task = runningTasks.get(position);
+        if (task != null) {
+            task.cancel(true);
+        }
+
+        task = new LoadResourceTask(position);
+        task.execute(slideImageIds[position]);
+    }
+
+    @Override
+    protected void onBitmapLoaded(int position, Bitmap bitmap) {
+        runningTasks.remove(position);
+        super.onBitmapLoaded(position, bitmap);
+    }
+
+    @Override
+    protected void onBitmapNotAvailable(int position) {
+        runningTasks.remove(position);
+        super.onBitmapNotAvailable(position);
+    }
+
+    //==============================================================================================
+    // ASYNC MANAGEMENT METHODS
+    //==
+
+    /**
+     * Stop all running download tasks. This method should be called when your activity gets
+     * stopped (in {#onStop})
+     */
+    public void stopAllDownloads() {
+        final int taskCount = runningTasks.size();
+        for (int i = 0; i < taskCount; ++i) {
+            int key = runningTasks.keyAt(i);
+            LoadResourceTask t = runningTasks.get(key);
+            t.cancel(true);
+        }
+        runningTasks.clear();
+    }
+
+    /**
+     * A task that downloads an image from the Internet and loads it into a bitmap
+     */
+    private class LoadResourceTask extends AsyncTask<Integer, Void, Bitmap> {
+
+        private int position;
+
+        public LoadResourceTask(int position) {
+            this.position = position;
+        }
+
+        protected Bitmap doInBackground(Integer... ids) {
+            Log.d("SlideShowView", "Download started for item " + position);
+
+            Bitmap bm = BitmapFactory.decodeResource(getContext().getResources(), ids[0], bitmapFactoryOptions);
+            if (isCancelled()) {
+                if (bm != null) bm.recycle();
+                return null;
+            }
+
+            return bm;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            if (result != null) {
+                onBitmapLoaded(position, result);
+            } else {
+                onBitmapNotAvailable(position);
+            }
         }
     }
 }
